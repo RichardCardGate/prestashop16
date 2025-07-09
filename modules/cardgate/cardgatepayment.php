84ed890f95eb839094a33b12f38267cc7fe17f95
richard <richard@cardgate.com>
2025-07-09 15:09:45 +0200

    Added: Payment method currency check.

--- modules/cardgate/cardgatepayment.php
+++ modules/cardgate/cardgatepayment.php
@@ -35,10 +35,32 @@ class CardgatePayment extends PaymentModule {
         return true;
     }
 
+    public function checkPaymentCurrency($currency,$payment_method) {
+        $strictly_euro = in_array($payment_method,['cardgateideal',
+            'cardgateidealqr',
+            'cardgatebancontact',
+            'cardgatebanktransfer',
+            'cardgatebillink',
+            'cardgatesofortbanking',
+            'cardgatedirectdebit',
+            'cardgateonlineueberweisen',
+            'cardgatespraypay']);
+        if ($strictly_euro && $currency != 'EUR') return false;
+
+        $strictly_pln = in_array($payment_method,['cardgateprzelewy24']);
+        if ($strictly_pln && $currency != 'PLN') return false;
+
+        return true;
+    }
+
     public function hookPayment( $params ) {
-        
+        $currency_order = new Currency( $params['cart']->id_currency );
+        $currency       = $currency_order->iso_code;
+        if (!$this->checkPaymentCurrency($currency,$this->name)) {
+            return;
+        }
+
         $display = Configuration::get('CARDGATE_PAYMENT_DISPLAY');
-   
         switch ($display){
             case 'textonly':
                 $paymentlogo = '';
